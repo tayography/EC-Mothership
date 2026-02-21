795b0499e0947510af8e2c2aa0a599748aa440b8
Taylor Isaacs <taylor@theendlesscreative.com>
2026-02-21 03:04:15 +0000

    File changes

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { createPageUrl } from "../utils";
+import { createPageUrl } from "../../utils";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   LayoutDashboard,
